e5b8586ae5aa29f9ff4b36efe22affccc57ae779
Rohit <rohitsrivas83@gmail.com>
2024-08-06 01:56:05 +0530

    Added email and password validator for incoming request

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,7 +4,21 @@ const UserController = require("../../controllers/user-controller");
 
 const router = express.Router();
 
-router.post('/signup',UserController.create);
-router.post('/signin',UserController.signIn);
+const { AuthRequestValidators } = require('../../middlewares/index');
+
+router.post(
+    '/signup',
+    AuthRequestValidators.validateUserAuth,
+    UserController.create
+);
+
+
+router.post(
+    '/signin',
+    AuthRequestValidators.validateUserAuth,
+    UserController.signIn
+);
+
+
 
 module.exports = router;
